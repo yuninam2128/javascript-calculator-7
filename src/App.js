5def45ba7b7810ee9973315e455c7bc7a39c8213
yuni <yuninam2128@gmail.com>
2025-02-07 11:37:07 +0900

    feat: 문자열 입출력 및 숫자 분리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class StringCalculator {
+  static add(numbers) {
+    const delimiters = [',', ':'];  // 기본 구분자 설정
+
+    let numArray = [numbers];   
+    for (const delimiter of delimiters) {
+      let tempArray = [];
+      for (const number of numArray) {
+        tempArray = tempArray.concat(number.split(delimiter));  // 구분자로 분리
+      }
+      numArray = tempArray;
+    }
+
+    return numArray;
+  }
+}
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Console.readLineAsync('입력: ');
+    const result = StringCalculator.add(input);
+    Console.print(`결과 : ${result}`);
+  }
 }
 
 export default App;
